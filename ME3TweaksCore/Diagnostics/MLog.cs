84c8edd8fdf0fe277edc63f9dc264e036306ac3c
Mgamerz <mgamerz2@gmail.com>
2021-11-13 19:36:38 -0700

    Add project files.

---
+++ ME3TweaksCore/Diagnostics/MLog.cs
@@ -0,0 +1,9 @@
+﻿namespace ME3TweaksCore.Diagnostics
+{
+    /// <summary>
+    /// Logger for ME3Tweaks software. Pass an ILogger into this class to set the logger up.
+    /// </summary>
+    public class MLog
+    {
+    }
+}
