f60e176d7f51d71c9f5145ea763dfe68844b2996
SirCxyrtyx <sircxyrtyx@gmail.com>
2022-05-28 11:29:50 -0700

    Make ME3TweaksCore buildable when there is a space in the Project's file path

--- ME3TweaksCore/Build/preparelocalizations.ps1
+++ ME3TweaksCore/Build/preparelocalizations.ps1
@@ -5,13 +5,15 @@ $filesToCompress = Get-ChildItem $localizationDir -Filter *.xaml
 
 
 foreach ($xaml in $filesToCompress){
-    $outname = $xaml.FullName + ".lzma"
+    $inname = "`"" + $xaml.FullName + "`""
+    $inname
+    $outname = "`"" + $xaml.FullName + ".lzma`""
     $outname
     $processOptions = @{
         FilePath = $lzmaExe
         Wait = $true
         NoNewWindow = $true
-        ArgumentList = "e",$xaml.FullName,$outname
+        ArgumentList = "e", $inname, $outname
     }
     $processOptions.FilePath
     Start-Process @processOptions
