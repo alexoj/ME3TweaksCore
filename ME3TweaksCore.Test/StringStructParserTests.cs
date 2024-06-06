8b92e678ec2283a23cedc9c9553116b862bfe9cc
Mgamerz <mgamerz@msn.com>
2024-06-06 08:05:30 -0600

    Add StringStructParser test. DLCRequirement now has 'keyed' version which uses a multivalue struct. Add some new multi-value struct parsing methods.

---
+++ ME3TweaksCore.Test/StringStructParserTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LegendaryExplorerCore.Misc;
+using ME3TweaksCore.Helpers;
+
+namespace ME3TweaksCore.Test
+{
+    internal class MultiParserResult
+    {
+        public string InputString { get; set; }
+        public CaseInsensitiveDictionary<List<string>> ExpectedResults { get; set; }
+        public char OpenChar { get; set; }
+        public char CloseChar { get; set; }
+        private MultiParserResult() { }
+
+        public static MultiParserResult Make(string input, char openChar, char closeChar,
+            List<(string key, string val)> results)
+        {
+            var mappedResults = new CaseInsensitiveDictionary<List<string>>();
+            foreach (var item in results)
+            {
+                if (!mappedResults.TryGetValue(item.key, out var list))
+                {
+                    list = new List<string>();
+                    mappedResults[item.key] = list;
+                }
+                list.Add(item.val);
+            }
+
+            return new MultiParserResult()
+            {
+                InputString = input,
+                OpenChar = openChar,
+                CloseChar = closeChar,
+                ExpectedResults = mappedResults,
+
+            };
+        }
+    }
+
+    [TestClass]
+    public class StringStructParserTests
+    {
+        internal static MultiParserResult[] Tests = [
+                // Easy
+                MultiParserResult.Make(@"(Attribute1=Value1)", '(', ')', [("Attribute1", "Value")]),
+                MultiParserResult.Make(@"[Attribute1=Value1]", '[', ']', [("Attribute1", "Value1")]),
+                MultiParserResult.Make(@"(Attribute1 = Value1)", '(', ')', [("Attribute1", "Value1")]),
+                MultiParserResult.Make(@"(Attribute1=Value1);", '(', ')', [("Attribute1", "Value1")]),
+            
+                // Medium
+                MultiParserResult.Make(@"(MedAttribute1=Kite, MedAttribute2=Bird)", '(', ')', 
+                    [("MedAttribute1", "Kite"), ("MedAttribute2", "Bird")]),
+                MultiParserResult.Make(@"(MedAttribute1=Kite, MedAttribute1=Bird)", '(', ')',
+                    [("MedAttribute1", "Kite"), ("MedAttribute1", "Bird")]),
+
+                // Hard
+                MultiParserResult.Make(@"(HardAttribute1=(Kite, Dog), HardAttribute1 = (Fly like a plane))", '(', ')',
+                    [("HardAttribute1", "(Kite, Dog)"), ("HardAttribute1", "(Fly like a plane)")]),
+
+                // Practical
+                MultiParserResult.Make(@"(MinVersion=0.99, Option=(Key=DB8CAA13, UIString=Expanded Galaxy Mod Normandy Module))", '(', ')',
+                    [("MinVersion", "0.99"), ("Option", "(Key=DB8CAA13, UIString=Expanded Galaxy Mod Normandy Module)")]),
+
+        ];
+
+        [TestMethod]
+        public void TestParsing()
+        {
+            // MULTI VALUE MAP TESTS
+            foreach (var test in Tests)
+            {
+                var split = StringStructParser.GetSplitMultiValues(test.InputString, true, test.OpenChar, test.CloseChar);
+                Assert.IsTrue(split.Count == test.ExpectedResults.Count);
+                Assert.IsTrue(split.Count == test.ExpectedResults.Count);
+                foreach (var key in split)
+                {
+                    if (test.ExpectedResults.TryGetValue(key.Key, out var list))
+                    {
+                        Assert.IsTrue(list.Count == key.Value.Count);
+                        foreach (var val in list)
+                        {
+                            Assert.IsTrue(list.Contains(val));
+                        }
+                    }
+                    else
+                    {
+                        Assert.Fail($"Expected results did not contain key {key.Key}");
+                    }
+                }
+            }
+        }
+    }
+}
--- ME3TweaksCore/Objects/DLCRequirement.cs
+++ ME3TweaksCore/Objects/DLCRequirement.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using LegendaryExplorerCore.Misc;
 using ME3TweaksCore.Diagnostics;
+using ME3TweaksCore.GameFilesystem;
+using ME3TweaksCore.Helpers;
 using ME3TweaksCore.Localization;
 using ME3TweaksCore.Misc;
 using ME3TweaksCore.Targets;
@@ -9,10 +12,13 @@ using ME3TweaksCore.Targets;
 namespace ME3TweaksCore.Objects
 {
     /// <summary>
-    /// Object that specifies a DLC foldername and a minimum version (as set in MetaCmm)
+    /// Object that specifies a DLC foldername and optionally set of conditions for determining if a DLC meets or does not meet them
     /// </summary>
     public class DLCRequirement
     {
+        private const string REQKEY_MINVERSION = @"minversion";
+        private const string REQKEY_DLCOPTIONKEY = @"optionkey";
+
         /// <summary>
         /// The DLC foldername to test for
         /// </summary>
@@ -24,6 +30,16 @@ namespace ME3TweaksCore.Objects
         public Version MinVersion { get; init; }
 
         /// <summary>
+        /// Chosen option requirements for DLC - CAN BE NULL to specify no options 
+        /// </summary>
+        public List<PlusMinusKey> DLCOptionKeys { get; init; }
+
+        /// <summary>
+        /// If this uses keyed conditions
+        /// </summary>
+        public bool IsKeyedVersion { get; set; }
+
+        /// <summary>
         /// Tests if this requirement is met against the given target
         /// </summary>
         /// <param name="target"></param>
@@ -33,33 +49,78 @@ namespace ME3TweaksCore.Objects
             installedDLC ??= target.GetInstalledDLC();
             if (installedDLC.Contains(DLCFolderName, StringComparer.InvariantCultureIgnoreCase))
             {
-                if (MinVersion == null)
+                // Check if there is anything beyond DLC present
+                if (!HasConditions())
                     return true; // No min version
 
                 var metas = target.GetMetaMappedInstalledDLC(installedDLC: installedDLC);
-                // if no metacmm file is found it will be mapped to null. In this case we just set it to version 0 because user did not use M3 and we have no way to determine version
-                if (metas.TryGetValue(DLCFolderName, out var meta) && Version.TryParse(meta?.Version ?? @"0.0.0.0", out var version))
+
+                // if no metacmm file is found it will be mapped to null.
+                if (metas.TryGetValue(DLCFolderName, out var meta) && meta != null)
                 {
-                    if (ProperVersion.IsGreaterThanOrEqual(version, MinVersion))
+                    // MINIMUM VERSION
+                    if (MinVersion != null)
                     {
-                        return true;
-                    }
-                    else
-                    {
-                        if (meta == null)
+                        if (Version.TryParse(meta.Version, out var version))
                         {
-                            MLog.Error($@"DLCRequirement not met: The version of the DLC mod {DLCFolderName} could not be determined; there is no mod manager metadata file with the DLC mod. Manually installed mods are not supported for version checks.");
+                            if (!ProperVersion.IsLessThan(version, MinVersion))
+                            {
+                                // Does not meet minimum version requirement
+                                MLog.Error($@"DLCRequirement not met: {version} is less than MinVersion {MinVersion}");
+                                return false;
+                            }
+
+                            // Version DLC Requirement Met
                         }
                         else
                         {
-                            MLog.Information($@"DLCRequirement not met: {version} is less than MinVersion {MinVersion}");
+                            MLog.Error($@"DLCRequirement not met: Could not read a semantic mod version of {DLCFolderName} from its _metacmm file. Cannot conduct minimum version requirement. Value: {meta.Version}");
+                            return false;
+                        }
+                    }
+
+                    // OPTION KEYS
+                    if (DLCOptionKeys != null)
+                    {
+                        foreach (var key in DLCOptionKeys)
+                        {
+                            if (!IsDLCOptionKeyRequirementMet(meta, key))
+                            {
+                                MLog.Error($@"DLCRequirement not met: DLC Option Key requirement is not met: {DLCFolderName} is not installed with optionkey value specified: {key}");
+                                return false;
+                            }
                         }
                     }
                 }
+                else
+                {
+                    MLog.Error($@"DLCRequirement not met: Information about the mod of the DLC mod {DLCFolderName} could not be determined; there is no mod manager metadata file with the DLC mod. Manually installed mods are not supported for extended DLC requirements.");
+                    return false;
+                }
             }
 
-            return false;
+            return true;
+        }
 
+        /// <summary>
+        /// Checks if this alternate meets the conditional DLC requirements for option keys in other mods.
+        /// </summary>
+        /// <param name="metaInfo"></param>
+        /// <returns></returns>
+        public bool IsDLCOptionKeyRequirementMet(MetaCMM meta, PlusMinusKey okd)
+        {
+            if (okd.IsPlus == true && !meta.OptionKeysSelectedAtInstallTime.Contains(okd.Key, StringComparer.InvariantCultureIgnoreCase))
+            {
+                // Option that must have been chosen was not.
+                return false;
+            }
+            if (okd.IsPlus == false && meta.OptionKeysSelectedAtInstallTime.Contains(okd.Key, StringComparer.InvariantCultureIgnoreCase))
+            {
+                // Option that must not have been chosen in another mod was chosen.
+                return false;
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -93,6 +154,7 @@ namespace ME3TweaksCore.Objects
 
             if (verStart == -1 || verEnd == -1 || verStart > verEnd)
             {
+                // Todo: UPDATE LOCALIZATION FOR THIS
                 throw new Exception(LC.GetString(LC.string_dlcRequirementInvalidParenthesis));
             }
 
@@ -111,6 +173,88 @@ namespace ME3TweaksCore.Objects
         }
 
         /// <summary>
+        /// Parses out a DLCRequirement string in the form of DLC_MOD_NAME[Attr=1, Attr2=Str] or DLC_MOD_NAME
+        /// </summary>
+        /// <param name="inputString">The input string</param>
+        /// <returns></returns>
+        /// <exception cref="Exception">If an error occurs parsing the string</exception>
+        public static DLCRequirement ParseRequirementKeyed(string inputString)
+        {
+            inputString = inputString.Trim();
+
+            if (inputString.Contains('(') || inputString.Contains(')'))
+            {
+                throw new Exception("DLCRequirements cannot contain ( or ) due to parser issues that would potentially break backwards compatibility if fixed. Use [ ] instead for conditions.");
+            }
+
+            var structStart = inputString.IndexOf('[');
+            var structEnd = inputString.IndexOf(']');
+
+            // No conditions.
+            if (structStart == -1 && structEnd == -1)
+                return new DLCRequirement() { DLCFolderName = inputString };
+
+            // Backwards ][.
+            if (structStart == -1 || structEnd == -1 || structStart > structEnd)
+            {
+                // Todo: UPDATE LOCALIZATION FOR THIS
+                throw new Exception(LC.GetString(LC.string_dlcRequirementInvalidParenthesis));
+            }
+
+            string attributeStruct = inputString.Substring(structStart + 1, structEnd - (structStart + 1));
+            var keyMap = StringStructParser.GetSplitMultiValues(attributeStruct, canBeCaseInsensitive: true);
+            if (keyMap.Count == 0)
+                throw new Exception($"Conditions in DLCRequirement must be in brackets, comma separated, and use key=value format. Bad value: {inputString}");
+
+            // Defaults
+            Version minVer = null;
+            List<PlusMinusKey> dlcOptionKeys = null;
+
+
+            // Read values
+            foreach (var key in keyMap)
+            {
+                // Conditions.
+                switch (key.Key)
+                {
+                    case DLCRequirement.REQKEY_MINVERSION:
+                        if (!Version.TryParse(key.Value[0], out minVer))
+                        {
+                            throw new Exception(LC.GetString(LC.string_interp_dlcRequirementInvalidBadVersion, key.Value));
+                        }
+                        break;
+                    case DLCRequirement.REQKEY_DLCOPTIONKEY:
+                        foreach (var value in key.Value)
+                        {
+                            var reqKey = new PlusMinusKey(value);
+                            if (reqKey.IsPlus == null)
+                            {
+                                throw new Exception($"Invalid DLCRequirement: DLCOptionKeys values must be prefixed with + or -. Bad value: {value}");
+                            }
+
+                            dlcOptionKeys ??= new List<PlusMinusKey>();
+                            dlcOptionKeys.Add(reqKey);
+                        }
+
+                        break;
+                    default:
+                        throw new Exception($"Unknown descriptor in DLCRequirement: {key.Key}");
+                }
+            }
+
+
+            return new DLCRequirement()
+            {
+                IsKeyedVersion = true,
+                DLCFolderName = inputString.Substring(0, structStart),
+                MinVersion = minVer,
+                DLCOptionKeys = dlcOptionKeys
+            };
+        }
+
+
+
+        /// <summary>
         /// Serializes this requirement back to the moddesc.ini format
         /// </summary>
         /// <returns>Moddesc.ini format of a DLC requirement</returns>
@@ -120,18 +264,46 @@ namespace ME3TweaksCore.Objects
             if (isSingle)
                 return $"?{Serialize(false)}"; // do not localize
 
-            if (MinVersion != null)
-                // Mod Manager 8.1: We serialize as [] instead of ()
-                // Since moddesc editor only supports latest version we use this instead.
-                return $@"{DLCFolderName}[{MinVersion}]";
-            return DLCFolderName;
+            if (IsKeyedVersion)
+            {
+                if (HasConditions())
+                {
+                    CaseInsensitiveDictionary<List<string>> keyMap = new CaseInsensitiveDictionary<List<string>>();
+                    if (MinVersion != null)
+                        keyMap[REQKEY_MINVERSION] = [MinVersion.ToString()];
+                    if (DLCOptionKeys != null)
+                        keyMap[REQKEY_DLCOPTIONKEY] = DLCOptionKeys.Select(x => x.ToString()).ToList();
+
+                    return $"{DLCFolderName}{StringStructParser.BuildCommaSeparatedSplitMultiValueList(keyMap, '[', ']')}";
+                }
+
+                // No Conditions.
+                return DLCFolderName;
+            }
+            else
+            {
+                // Pre Mod Manager 9
+                if (MinVersion != null)
+                {
+                    // Mod Manager 8.1: We serialize as [] instead of ()
+                    // Since moddesc editor only supports latest version we use this instead.
+                    return $@"{DLCFolderName}[{MinVersion}]";
+                }
+
+                // No conditions.
+                return DLCFolderName;
+            }
+        }
+
+        private bool HasConditions()
+        {
+            return DLCOptionKeys != null || MinVersion != null;
         }
 
         public override string ToString()
         {
-            if (MinVersion != null)
-                return $@"{DLCFolderName} ({MinVersion})";
-            return DLCFolderName;
+            // This is technically not always correct if it's a single
+            return Serialize(false);
         }
     }
 }
